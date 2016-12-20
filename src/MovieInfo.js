a4ca44c988f18007bc2ceb04c4a1a0ed35821934
levytsroman <r.levyts@gmail.com>
2016-12-20 02:11:01 -0500

    refactor movie info page structure

--- src/MovieInfo.js
+++ src/MovieInfo.js
@@ -7,24 +7,30 @@ class MovieInfo extends Component {
 
   render() {
     return(
-      <div>
+      <div className='container'>
         <a href='#' onClick={this.goBack.bind(this)}>go back</a>
         <h2>{this.props.thisMovie.Title}</h2>
+        <span>{this.props.thisMovie.Rated} | </span>
+        <span>{this.props.thisMovie.Runtime} | </span>
+        <span>{this.props.thisMovie.Country} | </span>
+        <span>{this.props.thisMovie.Genre} | </span>
+        <span>{this.props.thisMovie.Year}</span>
         <ul>
-          <li><img src={this.props.thisMovie.Poster} alt='poster'/></li>
-          <li>Actors: {this.props.thisMovie.Actors}</li>
-          <li>Awards: {this.props.thisMovie.Awards}</li>
-          <li>COuntry: {this.props.thisMovie.Country}</li>
-          <li>{this.props.thisMovie.Director}</li>
-          <li>{this.props.thisMovie.Genre}</li>
-          <li>{this.props.thisMovie.Language}</li>
-          <li><p>{this.props.thisMovie.Plot}</p></li>
-          <li>{this.props.thisMovie.Rated}</li>
-          <li>{this.props.thisMovie.Released}</li>
-          <li>{this.props.thisMovie.Runtime}</li>
-          <li>{this.props.thisMovie.Writer}</li>
-          <li>{this.props.thisMovie.Year}</li>
-
+          <li className='row'>
+            <img className='col m6' src={this.props.thisMovie.Poster} alt='poster'/>
+            <ul className='col m6'>
+              <li>Actors: {this.props.thisMovie.Actors}</li>
+              <li>Awards: {this.props.thisMovie.Awards}</li>
+              <li>Director: {this.props.thisMovie.Director}</li>
+              <li>Language: {this.props.thisMovie.Language}</li>
+              <li>Release date:{this.props.thisMovie.Released}</li>
+              <li>Writers: {this.props.thisMovie.Writer}</li>
+              <li>
+                <h5>Plot:</h5>
+                <p>{this.props.thisMovie.Plot}</p>
+              </li>
+            </ul>
+          </li>
         </ul>
       </div>
     )
