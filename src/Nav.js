a5dd1b2b3254f4a37ce1100de886027cfe6296dd
levytsroman <r.levyts@gmail.com>
2016-12-20 03:25:47 -0500

    change logo, tweak styles

--- src/Nav.js
+++ src/Nav.js
@@ -9,13 +9,13 @@ class Nav extends Component {
   render(){
     return(
       <nav>
-    <div className="nav-wrapper">
-      <a href="#" className="brand-logo"><img src='/logo.png' id='nav-logo' alt='logo'/></a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="back">back</a></li>
-      </ul>
-    </div>
-  </nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo"><img src='/logo.png' id='nav-logo' alt='logo'/></a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="back">back</a></li>
+          </ul>
+        </div>
+      </nav>
     )
   }
 }
